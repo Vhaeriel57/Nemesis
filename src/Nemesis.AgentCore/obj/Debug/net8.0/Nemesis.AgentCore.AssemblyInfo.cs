ca127314952ae2f143badff02fb133dfa199b616
Vhaeriel57 <maxime.szyjan@icloud.com>
2026-01-30 19:05:52 +0100

    :/

--- src/Nemesis.AgentCore/obj/Debug/net8.0/Nemesis.AgentCore.AssemblyInfo.cs
+++ src/Nemesis.AgentCore/obj/Debug/net8.0/Nemesis.AgentCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nemesis.AgentCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681b4f6f58f2cef4e4558a0fbfc175727e4ce57a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8e359107d798a14d1646a7213415917bfb7408")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nemesis.AgentCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nemesis.AgentCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
