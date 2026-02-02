8035af630573b2770e977e3c00d5bea9a054d127
Vhaeriel57 <maxime.szyjan@icloud.com>
2026-02-02 19:35:35 +0100

    :/

--- src/Nemesis.Shared/obj/Debug/net8.0/Nemesis.Shared.AssemblyInfo.cs
+++ src/Nemesis.Shared/obj/Debug/net8.0/Nemesis.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nemesis.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d352b276f6ccc5b894c402686baf261344ac05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64d608e09d54e5c7dd62d396bd6204cd08290c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nemesis.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nemesis.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
